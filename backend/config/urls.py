c362d4870097e6d23bfb0d619be4b8f50e4725c8
Christy Varghese <christy.varghese.tech@gmail.com>
2026-08-16 22:17:23 +0530

    Updated backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,19 +14,8 @@ urlpatterns = [
 
     path("api/accounts/", include("accounts.urls")),
 
-    path("api/token/", TokenObtainPairView.as_view()),
-    path("api/token/refresh/", TokenRefreshView.as_view()),
-    
-
-
-    path("admin/", admin.site.urls),
-
-    path("api/status/", api_status),
-
-    path("api/accounts/", include("accounts.urls")),
-
-    path("api/token/", TokenObtainPairView.as_view()),
-    path("api/token/refresh/", TokenRefreshView.as_view()),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
     path("api/", include("workouts.urls")),
 ]
